94de9046b2cdaf104537dd107f48dfab3b604038
Adi-0202 <adityasureshofficial1010@gmail.com>
2026-05-22 09:49:14 +0530

    Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,10 +6,26 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [mark, setMark] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const handleGood = () => setGood(good+1)
-  const handleNeutral = () => setNeutral(neutral+1)
-  const handleBad = () => setBad(bad+1)
+  const handleGood = () => {
+    setGood(good+1)
+    setTotal(total+1)
+    setMark(mark+1)
+    setPositive(positive+1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral+1)
+    setTotal(total+1)
+    setMark(mark+0)
+  }
+  const handleBad = () => {
+    setBad(bad+1)
+    setTotal(total+1)
+    setMark(mark-1)
+  }
 
   return(
     <div>
@@ -21,6 +37,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {total!=0? mark/total:"Null"}</p>
+      <p>positive {total!=0?(positive/total)*100:"Null"}%</p>
     </div>
   );
 }
