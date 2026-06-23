0683959a49f93d322f068cc14811881b0fa3bac9
Adi-0202 <adityasureshofficial1010@gmail.com>
2026-06-23 13:43:42 +0530

    Initial commit-2.1

---
+++ part 2/courseinfo/src/App.jsx
@@ -0,0 +1,62 @@
+const Course = ({course}) => {
+  return(
+    <div>
+      <Header head={course.name} />
+      <Content content={course.parts} />
+    </div>
+  )
+}
+
+const Header = ({head}) => {
+  return(
+    <div>
+      <h1>{head}</h1>
+    </div>
+  )
+}
+
+const Content = ({content}) => {
+  return(
+    <div>
+      {content.map(part => {
+        return <Part key={part.id} part={part} />
+      })}
+    </div>
+  )
+}
+
+const Part = ({part}) => {
+  return(
+    <div>
+      <p>{part.name} {part.exercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
