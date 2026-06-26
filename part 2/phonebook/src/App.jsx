aab2adf9af3e1850461a1466630d827a858fc264
Adi-0202 <adityasureshofficial1010@gmail.com>
2026-06-26 11:33:02 +0530

    Exercise 2.8

--- part 2/phonebook/src/App.jsx
+++ part 2/phonebook/src/App.jsx
@@ -1,14 +1,21 @@
+import { use } from 'react'
 import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: "040-1234567" }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   const handleNumbers = (event) => {
@@ -16,6 +23,7 @@ const App = () => {
     console.log(newName)
     const newObj={
       name:newName,
+      number:newNumber
     }
     let flag=false;
     for(const item of persons){
@@ -36,13 +44,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <>{persons.map(item => (
         <div key={item.name}>
-          {item.name}
+          {item.name} {item.number}
         </div>
       ))}</>
     </div>
