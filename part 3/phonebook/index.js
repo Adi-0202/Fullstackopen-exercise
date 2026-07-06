087eb3d78a8da715d0921ebfdcc5f11e3ec9fd24
Adi-0202 <adityasureshofficial1010@gmail.com>
2026-07-06 06:30:05 +0530

    Exercise 3.2

--- part 3/phonebook/index.js
+++ part 3/phonebook/index.js
@@ -28,6 +28,15 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/info', (request, response) => {
+    const length=persons.length
+    const time= new Date()
+    response.send(
+        `<p>Phonebook has info for ${length} people</p>
+         <p>${time}</p>`
+    )
+})
+
 const PORT=3001
 app.listen(PORT,
     console.log(`Server is running on ${PORT}`)
