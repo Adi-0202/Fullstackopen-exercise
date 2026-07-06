af5668f3dfa44c41839cfeb860bc0688418922f4
Adi-0202 <adityasureshofficial1010@gmail.com>
2026-07-06 06:42:08 +0530

    Exercise 3.4

--- part 3/phonebook/index.js
+++ part 3/phonebook/index.js
@@ -48,6 +48,12 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id=request.params.id
+    persons=persons.filter(item => item.id!=id)
+    response.status(204).end()
+})
+
 const PORT=3001
 app.listen(PORT,
     console.log(`Server is running on ${PORT}`)
