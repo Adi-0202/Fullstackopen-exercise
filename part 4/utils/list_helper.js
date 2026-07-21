bd7f5bfa18d57af44c71daeb1e891365accb056b
Adi-0202 <adityasureshofficial1010@gmail.com>
2026-07-21 08:59:34 +0530

    testing favourite blog using max likes

--- part 4/utils/list_helper.js
+++ part 4/utils/list_helper.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const dummy = (blogs) => {
     return 1
 }
@@ -10,4 +11,18 @@ const totalLikes = (array) => {
     return array.length===0? array.length: array.reduce(totalLikes, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favouriteBlog = (array) => {
+    let max = 0
+    let maxIndex = 0
+
+    for (const [index, item] of array.entries()) {
+        if (item.likes > max) {
+            max = item.likes
+            maxIndex = index
+        }
+    }
+
+    return array.length===0? array.length: array[maxIndex]
+}
+
+module.exports = { dummy, totalLikes, favouriteBlog, mostBlogs }
