27b777309fdd8ce644477a45eacbcb30faacbaac
Adi-0202 <adityasureshofficial1010@gmail.com>
2026-08-01 22:33:45 +0530

    Implemented users DB and tested POST and GET through both browser and REST

--- part 4/app.js
+++ part 4/app.js
@@ -4,6 +4,7 @@ const config=require('./utils/config')
 const logger=require('./utils/logger')
 const middleware=require('./utils/middleware')
 const blogsRouter=require('./controllers/blogs')
+const userRouter=require('./controllers/users')
 
 const app=express()
 
@@ -19,6 +20,7 @@ mongoose
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', userRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorhandler)
 module.exports=app
---
+++ part 4/controllers/users.js
@@ -0,0 +1,28 @@
+const bcrypt=require('bcrypt')
+const userRouter=require('express').Router()
+const User=require('../models/users')
+
+userRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    response.json(users)
+})
+
+userRouter.post('/', async (request, response) => {
+    const { username, name, password }=request.body
+    const saltRounds=10
+    const passwordHash= await bcrypt.hash(password, saltRounds)
+    const newUser=new User({
+        username,
+        name,
+        passwordHash,
+    })
+    const savedUser=await newUser.save()
+    response.status(201).json(savedUser)
+})
+
+userRouter.delete('/:id', async (request, response) => {
+    const user = await User.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+})
+
+module.exports=userRouter
